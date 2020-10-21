4d013416e28b7fd9538d05e6bf94c3090a946ebf
MrRiverstones <mr_riverstones@163.com>
2020-10-21 16:25:42 +0800

    修改tab为空格

--- bc_3.1/WORK/test1.c
+++ bc_3.1/WORK/test1.c
@@ -2,7 +2,7 @@
 #include"print1.h"
 void main()
 {
-	printf('test1.c - print1_val: %d', print1_val);
-	printf('test1.c - print2_val: %d', print2_val);
-	while(1);
+  printf('test1.c - print1_val: %d', print1_val);
+  printf('test1.c - print2_val: %d', print2_val);
+  while(1);
 }
